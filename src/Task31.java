38dfacabce8e8def25e30cccc89470cdf1af233b
csizmatimea <35235498+csizmatimea@users.noreply.github.com>
2022-11-19 10:32:39 +0100

    Tömb  Fiat

--- src/Task31.java
+++ src/Task31.java
@@ -1,19 +1,22 @@
 
 
-    import java.util.Random;
+import java.util.Random;
 
-    public class Task31 {
-        public static void main(String[] args)
-        {
-            int[] numbers = new int[100];
+public class Task31 {
+    public static void main(String[] args) {
+        int[] numbers = new int[50];
+        int numbersIndex = 0;
 
-            for (int i = 0; i < numbers.length; i++) {
-                numbers[i] = i+1;
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                numbers[numbersIndex] = i;
+                numbersIndex++;
             }
-
-            for (int i = 0; i < numbers.length; i++) {
-                System.out.print(numbers[i] + " ");
-            }
-
         }
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+
     }
+}
