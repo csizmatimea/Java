c3f2ab163817aa8b3ed9453e8c252e035bb38d31
csizmatimea <35235498+csizmatimea@users.noreply.github.com>
2022-12-05 14:32:01 +0100

    Tömb  Fiat

---
+++ src/Task3b.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task3b {
+    public static void main(String[] args) {
+        System.out.println("The numbers in the args are: ");
+        System.out.println(args[0] + " and " + args[1]);
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        int result = (a * 3) * b; /// “a” és “b” szorzata, miután megszoroztuk az “a”-t 3
+        System.out.println("The result is: " + result);
+    }
+}
